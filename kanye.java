467facffc8eddb1d2d4317f87d69ff67a85636c2
babbo65 <113200930+babbo65@users.noreply.github.com>
2023-06-27 20:35:55 -0400

    Update kanye.java

--- kanye.java
+++ kanye.java
@@ -1,4 +1,4 @@
-//pulls Kanye twitter quote then prints in terminal
+//pulls Kanye twitter quote then prints in terminal, uses kanye.rest api
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
